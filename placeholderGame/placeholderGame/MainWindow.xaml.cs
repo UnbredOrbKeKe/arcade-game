dc1727159df96475f3476b9c14b58a6dfd355be4
Gewoon-Dennis <geertsma.dennis@gmail.com>
2021-10-25 10:40:19 +0200

    Asteroide en Puntensysteem
    Er zijn nu 5 enemy's die elk een verschillende score geven en de meest zeldzame geeft een powerup waardoor je sneller kan schieten.
    Daarnaast kunnen de spelers elkaar nu raken waardoor je tijdelijk niet kan schieten en zijn er ook asteroides in het spel die wanneer ze je raken er ook voor zorgen dat je niet kan schieten.

--- placeholderGame/placeholderGame/MainWindow.xaml.cs
+++ placeholderGame/placeholderGame/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Media;
 using Microsoft.Win32;
-
+using System.Diagnostics;
 using System.Windows.Threading; // add this for the timer
 
 namespace PlaceholderGame
@@ -30,17 +30,23 @@ namespace PlaceholderGame
 
 		Random rand = new Random();
 
+		int TimeLeft = 180;
 		
 		int player1Speed = 10;
 		int player2Speed = 10;
-		int limit = 50;
 		int score1 = 0;
 		int coins1 = 0;
 		int coins2 = 0;
 		int score2 = 0;
 
-		Rect player1HitBox;
-		Rect player2HitBox;
+		int Player1ShootDelay = 250;
+		int Player2ShootDelay = 250;
+		int PlayerDisableShootTime = 5000;
+
+		int PowerUpTime = 3000;
+
+		bool Player1CanShoot = true;
+		bool Player2CanShoot = true;
 
 		private MediaPlayer music = new MediaPlayer();
 
@@ -85,13 +91,17 @@ namespace PlaceholderGame
 
 		}
 
-
-		public void GameEngine(object sender, EventArgs e)
+        #region GameEngine
+        public void GameEngine(object sender, EventArgs e)
 		{
 			scoreText1.Content = "Score: " + score1;
 			scoreText2.Content = "Score: " + score2;
+			
+			Rect player1HitBox = new Rect(Canvas.GetLeft(player1), Canvas.GetTop(player1), player1.Width, player1.Height); //hitbox player rechts
+			Rect player2HitBox = new Rect(Canvas.GetLeft(player2), Canvas.GetTop(player2), player2.Width, player2.Height); //hitbox player links
 
-
+			EnemyAsteroid(rand.Next(1, 51));
+            
 			if (moveLeft1 == true && Canvas.GetTop(player1) > 0)
 			{
 				Canvas.SetTop(player1, Canvas.GetTop(player1) - player1Speed);
@@ -113,10 +123,101 @@ namespace PlaceholderGame
 			foreach (var x in MyCanvas.Children.OfType<Rectangle>())
 			{
 
-				if (x is Rectangle && (string)x.Tag == "bullet1")
+				if (x is Rectangle && (string)x.Tag == "bullet2")
 				{
 
 					Canvas.SetLeft(x, Canvas.GetLeft(x) + 20);
+
+
+					Rect bullet = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+
+					if (Canvas.GetLeft(x) < 10)
+					{
+						itemstoremove.Add(x);
+					}
+
+					if (bullet.IntersectsWith(player1HitBox))
+					{
+						itemstoremove.Add(x);
+						Player1CanShoot = false;
+						Task.Delay(PlayerDisableShootTime).ContinueWith(_ => {Player1CanShoot = true; });
+						
+					}
+
+					foreach (var y in MyCanvas.Children.OfType<Rectangle>())
+					{
+
+						if (y is Rectangle && ((string)y.Tag == "EnemyTop" || (string)y.Tag == "EnemyBottom"))
+						{
+							Rect enemy = new Rect(Canvas.GetLeft(y), Canvas.GetTop(y), y.Width, y.Height);
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 82)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score2 += 2;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 81)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score2 += 4;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 80)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score2 += 6;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 79)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score2 += 8;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 78)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score2 += 10;
+
+								Player2ShootDelay = 10;
+								Task.Delay(PowerUpTime).ContinueWith(_ => { Player2ShootDelay = 250; });
+							}
+						}
+
+						#region Bullet2 Intersects with asteroid
+						if (y is Rectangle && ((string)y.Tag == "asteroidLinks" || (string)y.Tag == "asteroidRechts"))
+						{
+							Rect asteroid = new Rect(Canvas.GetLeft(y), Canvas.GetTop(y), y.Width, y.Height);
+
+							if (bullet.IntersectsWith(asteroid))
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score2++;
+							}
+						}
+						#endregion
+					}
+				}
+			}
+
+
+			
+
+			foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+			{
+
+				if (x is Rectangle && (string)x.Tag == "bullet1")
+				{
+
+					Canvas.SetLeft(x, Canvas.GetLeft(x) - 20);
 
 
 					Rect bullet = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
@@ -128,6 +229,14 @@ namespace PlaceholderGame
 						itemstoremove.Add(x);
 					}
 
+					if (bullet.IntersectsWith(player2HitBox))
+					{
+						itemstoremove.Add(x);
+						Player2CanShoot = false;
+						Task.Delay(PlayerDisableShootTime).ContinueWith(_ => { Player2CanShoot = true; });
+
+					}
+
 
 					foreach (var y in MyCanvas.Children.OfType<Rectangle>())
 					{
@@ -136,102 +245,144 @@ namespace PlaceholderGame
 						{
 							Rect enemy = new Rect(Canvas.GetLeft(y), Canvas.GetTop(y), y.Width, y.Height);
 
-							if (bullet.IntersectsWith(enemy))
+							if (bullet.IntersectsWith(enemy) && y.Width == 82)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score1 += 2;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 81)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score1 += 4;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 80)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score1 += 6;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 79)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score1 += 8;
+							}
+
+							if (bullet.IntersectsWith(enemy) && y.Width == 78)
+							{
+								itemstoremove.Add(x);
+								itemstoremove.Add(y);
+								score1 += 10;
+								Player1ShootDelay = 10;
+								Task.Delay(PowerUpTime).ContinueWith(_ => { Player1ShootDelay = 250; });
+							}
+						}
+                        #region Bullet1 Intersects with asteroid
+                        if (y is Rectangle && ((string)y.Tag == "asteroidLinks" || (string)y.Tag == "asteroidRechts"))
+						{
+							Rect asteroid = new Rect(Canvas.GetLeft(y), Canvas.GetTop(y), y.Width, y.Height);
+
+							if (bullet.IntersectsWith(asteroid))
 							{
 
 								itemstoremove.Add(x);
 								itemstoremove.Add(y);
 								score1++;
+
 							}
 						}
+                        #endregion
+                    }
+                }
+                #region Enemy Deleted When out of frame
+                if ((string)x.Tag == "EnemyTop")
+				{
+					Canvas.SetTop(x, Canvas.GetTop(x) + EnemySpeed);
+					Rect enemy = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
 
+					if (Canvas.GetTop(x) + 20 > 720)
+					{
+						itemstoremove.Add(x);
+					}
+				}
+
+				if ((string)x.Tag == "EnemyBottom")
+				{
+					Canvas.SetTop(x, Canvas.GetTop(x) - EnemySpeed);
+					Rect enemy = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+					if (Canvas.GetTop(x) + 20 < 0)
+					{
+						itemstoremove.Add(x);
+					}
+				}
+				#endregion
+
+				#region asteroidRechts Intersects
+				if (x is Rectangle && (string)x.Tag == "asteroidRechts")
+				{
+					Canvas.SetLeft(x, Canvas.GetLeft(x) + EnemySpeed);
+					Rect asteroid = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+					if (Canvas.GetLeft(x) + 20 > 1280)
+					{
+						itemstoremove.Add(x);
+					}
+
+					if (asteroid.IntersectsWith(player1HitBox))
+					{
+						itemstoremove.Add(x);
+						Player1CanShoot = false;
+						Task.Delay(PlayerDisableShootTime).ContinueWith(_ => { Player1CanShoot = true; });
 
 					}
 				}
+				#endregion
+
+				#region asteroidLinks Intersects
+
+				if (x is Rectangle && (string)x.Tag == "asteroidLinks")
+				{
+					Canvas.SetLeft(x, Canvas.GetLeft(x) - EnemySpeed);
+					Rect asteroid = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+					if (Canvas.GetLeft(x) + 20 < 0)
+					{
+						itemstoremove.Add(x);
+					}
+
+					if (asteroid.IntersectsWith(player2HitBox))
+					{
+						itemstoremove.Add(x);
+						Player2CanShoot = false;
+						Task.Delay(PlayerDisableShootTime).ContinueWith(_ => { Player2CanShoot = true; });
+
+					}
+				}
+                #endregion
+            }
+
+            foreach (Rectangle r in itemstoremove)
+			{
+				MyCanvas.Children.Remove(r);
 			}
 
-
-			
-
-				foreach (var x in MyCanvas.Children.OfType<Rectangle>())
-				{
-
-					if (x is Rectangle && (string)x.Tag == "bullet2")
-					{
-
-						Canvas.SetLeft(x, Canvas.GetLeft(x) - 20);
-
-
-						Rect bullet = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
-
-
-						if (Canvas.GetLeft(x) < 10)
-						{
-
-							itemstoremove.Add(x);
-						}
-
-
-						foreach (var y in MyCanvas.Children.OfType<Rectangle>())
-						{
-
-							if (y is Rectangle && ((string)y.Tag == "EnemyTop" || (string)y.Tag == "EnemyBottom"))
-							{
-								Rect enemy = new Rect(Canvas.GetLeft(y), Canvas.GetTop(y), y.Width, y.Height);
-
-								if (bullet.IntersectsWith(enemy))
-								{
-
-									itemstoremove.Add(x);
-									itemstoremove.Add(y);
-									score2++;
-								}
-							}
-
-
-						}
-					}
-
-					if ((string)x.Tag == "EnemyTop")
-					{
-						Canvas.SetTop(x, Canvas.GetTop(x) + EnemySpeed);
-						Rect enemy = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
-
-						if (Canvas.GetTop(x) + 20 > 720)
-						{
-							itemstoremove.Add(x);
-						}
-					}
-
-					if ((string)x.Tag == "EnemyBottom")
-					{
-						Canvas.SetTop(x, Canvas.GetTop(x) - EnemySpeed);
-						Rect enemy = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
-
-						if (Canvas.GetTop(x) + 20 < 0)
-						{
-							itemstoremove.Add(x);
-						}
-					}
-				}
-				player1HitBox = new Rect(Canvas.GetLeft(player1), Canvas.GetTop(player1), player1.Width, player1.Height);
-
-
-				foreach (Rectangle r in itemstoremove)
-				{
-					MyCanvas.Children.Remove(r);
-				}
-
-				EnemySpawnCount--;
-				if (EnemySpawnCount < 0)
-				{
-					MakeEnemies();
-					EnemySpawnCount = EnemySpawnMax;
-				}
+			EnemySpawnCount--;
+			if (EnemySpawnCount < 0)
+			{
+				MakeEnemies();
+				EnemySpawnCount = EnemySpawnMax;
 			}
-		
+		}
+        #endregion
 
-		private void OnKeyDown(object sender, KeyEventArgs e)
+        #region OnKeyDown
+        private void OnKeyDown(object sender, KeyEventArgs e)
 		{
 			if (e.Key == Key.Up)
 			{
@@ -250,9 +401,13 @@ namespace PlaceholderGame
 			{
 				moveRight2 = true;
 			}
-		}
 
-		private void OnKeyUp(object sender, KeyEventArgs e)
+
+		}
+        #endregion
+
+        #region OnKeyUp
+        private void OnKeyUp(object sender, KeyEventArgs e)
 		{
 			if (e.Key == Key.Up)
 			{
@@ -272,71 +427,52 @@ namespace PlaceholderGame
 				moveRight2 = false;
 			}
 
-			if (e.Key == Key.RightShift)
+			if (e.Key == Key.RightShift && Player1CanShoot == true)
 			{
-				Rectangle newBullet = new Rectangle
-				{
-					Tag = "bullet2",
-					Height = 5,
-					Width = 5,
-					Fill = Brushes.White,
-					Stroke = Brushes.Red
-				};
-
+				Player1Shoot();
 				
-				Canvas.SetTop(newBullet, Canvas.GetTop(player1) + player1.Height / 2);
-				
-				Canvas.SetLeft(newBullet, Canvas.GetLeft(player1) - newBullet.Width);
-				
-				MyCanvas.Children.Add(newBullet);
 			}
 
-			if (e.Key == Key.LeftShift)
+			if (e.Key == Key.Space && Player2CanShoot == true)
 			{
-				Rectangle newBullet = new Rectangle
-				{
-					Tag = "bullet1",
-					Height = 5,
-					Width = 5,
-					Fill = Brushes.White,
-					Stroke = Brushes.Red
-				};
-
-
-				Canvas.SetTop(newBullet, Canvas.GetTop(player2) + player2.Height / 2);
-
-				Canvas.SetLeft(newBullet, newBullet.Width + Canvas.GetLeft(player2) + player2.Width);
-
-				MyCanvas.Children.Add(newBullet);
+				Player2Shoot();
 			}
+
 		}
+        #endregion
 
-
-		private void MakeEnemies()
+        #region MakeEnemies
+        private void MakeEnemies()
         {
 			ImageBrush EnemyType = new ImageBrush();
 			int EnemyTypeCounter = rand.Next(1, 16);
-
+			int Dimension = 0;
+			
 			switch (EnemyTypeCounter)
             {
 				case 1: case 2: case 3: case 4: case 5:
 					EnemyType.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/1.png"));
+					Dimension = 82;
 					break;
 
 				case 6: case 7: case 8: case 9:
 					EnemyType.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/2.png"));
+					Dimension = 81;
 					break;
 
 				case 10: case 11: case 12:
 					EnemyType.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/3.png"));
+					Dimension = 80;
 					break;
 
 				case 13: case 14:
 					EnemyType.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/4.png"));
+					Dimension = 79;
 					break;
 
 				case 15:
 					EnemyType.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/5.png"));
+					Dimension = 78;
 					break;
 
 			}
@@ -349,8 +485,8 @@ namespace PlaceholderGame
 					Rectangle NewEnemyTop = new Rectangle
 					{
 						Tag = "EnemyTop",
-						Height = 80,
-						Width = 80,
+						Height = Dimension,
+						Width = Dimension,
 						Fill = EnemyType
 					};
 
@@ -363,8 +499,8 @@ namespace PlaceholderGame
 					Rectangle NewEnemyBottom = new Rectangle
 					{
 						Tag = "EnemyBottom",
-						Height = 80,
-						Width = 80,
+						Height = Dimension,
+						Width = Dimension,
 						Fill = EnemyType
 					};
 
@@ -378,6 +514,109 @@ namespace PlaceholderGame
 			GC.Collect();
 
         }
+        #endregion
 
-	}
+		#region Player1Shoot
+        private async void Player1Shoot()
+		{
+			Player1CanShoot = false;
+
+			Rectangle newBullet = new Rectangle
+			{
+				Tag = "bullet1",
+				Height = 5,
+				Width = 5,
+				Fill = Brushes.White,
+				Stroke = Brushes.Red
+			};
+
+
+			Canvas.SetTop(newBullet, Canvas.GetTop(player1) + player1.Height / 2);
+
+			Canvas.SetLeft(newBullet, Canvas.GetLeft(player1) - newBullet.Width);
+
+			MyCanvas.Children.Add(newBullet);
+
+			await Task.Delay(Player1ShootDelay);
+
+			Player1CanShoot = true;
+
+		}
+        #endregion
+
+        #region Player2Shoot
+        private async void Player2Shoot()
+		{
+			Player2CanShoot = false;
+
+			Rectangle newBullet = new Rectangle
+			{
+				Tag = "bullet2",
+				Height = 5,
+				Width = 5,
+				Fill = Brushes.White,
+				Stroke = Brushes.Red
+			};
+
+
+			Canvas.SetTop(newBullet, Canvas.GetTop(player2) + player2.Height / 2);
+
+			Canvas.SetLeft(newBullet, newBullet.Width + Canvas.GetLeft(player2) + player2.Width);
+
+			MyCanvas.Children.Add(newBullet);
+
+			await Task.Delay(Player2ShootDelay);
+
+			Player2CanShoot = true;
+		}
+        #endregion
+
+        #region Asteroid Spawning
+        private void EnemyAsteroid(int Spawn)
+		{
+			ImageBrush Asteroid = new ImageBrush();
+			Asteroid.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/asteroid.png"));
+
+			int AsteroidDirection = rand.Next(1,3);
+			if (Spawn == 3)
+			{ 
+				switch(AsteroidDirection)
+				{
+					case 1:
+						Rectangle AsteroidLinks = new Rectangle
+						{
+							Tag = "asteroidLinks",
+							Height = 20,
+							Width = 20,
+							Fill = Asteroid
+						};
+						Canvas.SetTop(AsteroidLinks, rand.Next(100, 620));
+
+						Canvas.SetLeft(AsteroidLinks, 640);
+
+						MyCanvas.Children.Add(AsteroidLinks);
+						break;
+
+
+					case 2:
+						Rectangle AsteroidRechts = new Rectangle
+						{
+							Tag = "asteroidRechts",
+							Height = 20,
+							Width = 20,
+							Fill = Asteroid
+						};
+						Canvas.SetTop(AsteroidRechts, rand.Next(100, 620));
+
+						Canvas.SetLeft(AsteroidRechts, 640);
+
+						MyCanvas.Children.Add(AsteroidRechts);
+						break;
+				}
+			}
+		}
+		#endregion
+
+    }
+
 }
